a158c303f12d8a6de0e31aea09cb153e45db2f4b
Krasney5 <nikitavasaev2008@gmail.com>
2025-02-08 00:01:43 +0300

    отображение баланса

--- Assets/Scripts/Balance/BalanceCounter.cs
+++ Assets/Scripts/Balance/BalanceCounter.cs
@@ -1,20 +1,35 @@
+using System;
+using UI;
 using UnityEngine;
 
 namespace Balance
 { 
-    public class BalanceCounter : MonoBehaviour
+    public class BalanceCounter : MonoBehaviour, ITextUser
     {
         [SerializeField] private long _balance;
+        public event Action<string> Changed;
 
+
+        private void Start()
+        {
+            BalanceIncrease(0);
+        }
 
         public void BalanceIncrease(int count)
         {
             _balance += count;
+            Changed?.Invoke(_balance.ToString());
         }
 
         public void BalanceReduction(int count)
         {
+            if (count > _balance) return;
+         
             _balance -= count;
+            Changed?.Invoke(_balance.ToString());
+            
         }
+        
+        
     }
 }
